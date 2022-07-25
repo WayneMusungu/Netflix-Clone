1d33219e1bb42046f7a2acf28730ccea8529ceef
WayneMusungu <waynemuyera17@gmail.com>
2022-07-25 08:39:01 +0300

    add login and register temlplates and their views

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render, render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import CreateUserForm, LoginUserForm
 
 
 # Create your views here.
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+    if request.method =='POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            messages.success(request, f'Account created for {username}')
+
+            return redirect('login')
+
+    return render(request, 'register.html', {'form': form})
+
+def login(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = LoginUserForm()
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html', {'form':form})
+
+
 
 def home(request):
     return render(request, 'index.html')
